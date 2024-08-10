2c643bbaef654c40a9ac40dd47c2fdf25e4b66c8
fmelihh <furkanmelihercan.98@gmail.com>
2024-08-10 21:56:18 +0300

    feat: swagger configurations

--- go-backend/cmd/product-service/main.go
+++ go-backend/cmd/product-service/main.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"net/http"
 
+	_ "product-api-go/cmd/product-service/docs"
+
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 type Product struct {
@@ -17,36 +20,77 @@ type Product struct {
 	location    string  `json:"location"`
 }
 
-func getProduct(w http.ResponseWriter, r *http.Request) {
+// @Summary Get Product
+// @Description get product
+// @Produce  json
+// @Success 200
+// @Router /product/{product-id} [get]
+func GetProduct(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("get product")
 }
 
-func addProduct(w http.ResponseWriter, r *http.Request) {
+// @Summary Insert Product
+// @Description insert product
+// @Produce  json
+// @Success 200
+// @Router /product [post]
+func AddProduct(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("post product")
 }
 
-func listProduct(w http.ResponseWriter, r *http.Request) {
+// @Summary List Products
+// @Description list inserted products
+// @Produce  json
+// @Success 200 {array} Product
+// @Router /product/list [get]
+func ListProduct(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("list product")
 }
 
-func deleteProduct(w http.ResponseWriter, r *http.Request) {
+// @Summary Delete Product
+// @Description delete inserted product
+// @Produce  json
+// @Success 200
+// @Router /product/ [delete]
+func DeleteProduct(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("delete product")
 }
 
-func updateProduct(w http.ResponseWriter, r *http.Request) {
+// @Summary Update Product
+// @Description update inserted product
+// @Produce  json
+// @Success 200
+// @Router /product/ [put]
+func UpdateProduct(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("update product")
 }
 
+// @title Product API
+// @version 1.0
+// @description CRUD Operations for the products
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8000
 func main() {
-	router := mux.NewRouter()
+	// Swagger URL: http://localhost:8000/docs/index.html
+	router := mux.NewRouter().StrictSlash(true)
+	router.PathPrefix("/docs/").Handler(httpSwagger.WrapHandler)
 
-	productRouter := router.PathPrefix("/products").Subrouter()
-	productRouter.HandleFunc("", addProduct).Methods("POST")
-	productRouter.HandleFunc("", updateProduct).Methods("PUT")
-	productRouter.HandleFunc("/list", listProduct).Methods("GET")
-	productRouter.HandleFunc("", deleteProduct).Methods("DELETE")
-	productRouter.HandleFunc("/{productID}", getProduct).Methods("GET")
+	productRouter := router.PathPrefix("/products/").Subrouter()
+	productRouter.HandleFunc("", AddProduct).Methods("POST")
+	productRouter.HandleFunc("", UpdateProduct).Methods("PUT")
+	productRouter.HandleFunc("/list", ListProduct).Methods("GET")
+	productRouter.HandleFunc("", DeleteProduct).Methods("DELETE")
+	productRouter.HandleFunc("/{productID}", GetProduct).Methods("GET")
 
+	log.Println("Server Starting on 8000")
 	err := http.ListenAndServe(":8000", router)
 	if err != nil {
 		log.Fatalln("There's an error with the server", err)
